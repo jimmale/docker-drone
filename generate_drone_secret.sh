f5388265cd065e3f60262a9a5c618d20b944acac
Jim Male <jmale1@gmail.com>
2018-09-30 11:00:13 -0500

    changed to a secret that's at least 4096 bits

--- generate_drone_secret.sh
+++ generate_drone_secret.sh
@@ -3,4 +3,4 @@ rm ./drone_secret.secret
 touch ./drone_secret.secret
 chmod 600 ./drone_secret.secret
 echo -n "DRONE_SECRET=" > ./drone_secret.secret
-dd if=/dev/urandom bs=72 count=1 2> /dev/null | base64  >> ./drone_secret.secret
+dd if=/dev/urandom bs=540 count=1 2> /dev/null | base64  >> ./drone_secret.secret
