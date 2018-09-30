8276bca5a98a02776f56c1446c9af18c2d4c2138
Jim Male <jmale1@gmail.com>
2018-09-30 10:48:00 -0500

    got them working :)

--- generate_drone_secret.sh
+++ generate_drone_secret.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo -n "DRONE_SECRET=" > ./drone_secret.env
-dd if=/dev/urandom bs=72 count=1 | base64  >> ./drone_secret.env
+echo -n "DRONE_SECRET=" > ./drone_secret.secret
+dd if=/dev/urandom bs=72 count=1 2> /dev/null | base64  >> ./drone_secret.secret
